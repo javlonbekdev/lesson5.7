708addc9bb1bc7233cdac28a77e963d958cfbf41
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-01-26 11:33:29 +0500

    Initial Commit

---
+++ lesson5.7/lesson5_7App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson5_7App.swift
+//  lesson5.7
+//
+//  Created by Javlonbek on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct lesson5_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
